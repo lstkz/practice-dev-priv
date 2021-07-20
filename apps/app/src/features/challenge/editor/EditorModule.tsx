0de853d3fe7ff9e8818bf787c7c951660f21a3c7
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2021-07-20 17:09:01 +0200

    IDE: save file and tab state (#25)

--- apps/app/src/features/challenge/editor/EditorModule.tsx
+++ apps/app/src/features/challenge/editor/EditorModule.tsx
@@ -4,6 +4,7 @@ import { createModuleContext, useActions, useImmer } from 'context-api';
 import React from 'react';
 import { CodeEditor } from 'src/components/CodeEditor/CodeEditor';
 import { ElementInfo, FileInfo, FileService } from './FileService';
+import { EditorStateService } from './EditorStateService';
 
 interface Actions {
   load: (container: HTMLDivElement) => void;
@@ -17,10 +18,11 @@ interface OpenedTab {
 }
 
 interface State {
-  activeFile: string | null;
+  activeTabId: string | null;
   isLoaded: boolean;
   elements: ElementInfo[];
   tabs: OpenedTab[];
+  dirtyMap: Record<string, boolean>;
 }
 
 const [Provider, useContext] = createModuleContext<State, Actions>();
@@ -44,15 +46,28 @@ export function EditorModule(props: EditorModuleProps) {
   const { challengeId, children } = props;
   const [state, setState, getState] = useImmer<State>(
     {
-      activeFile: null,
+      activeTabId: null,
       isLoaded: false,
       elements: [],
       tabs: [],
+      dirtyMap: {},
     },
     'EditorModule'
   );
   const codeEditor = React.useMemo(() => new CodeEditor(), []);
   const fileService = React.useMemo(() => new FileService(challengeId), []);
+  const editorStateService = React.useMemo(
+    () => new EditorStateService(challengeId),
+    []
+  );
+
+  const syncTabs = () => {
+    const { activeTabId, tabs } = getState();
+    editorStateService.updateTabsState({
+      activeTabId,
+      tabs,
+    });
+  };
 
   const actions = useActions<Actions>({
     load: async container => {
@@ -68,6 +83,7 @@ export function EditorModule(props: EditorModuleProps) {
           'https://cdn.jsdelivr.net/npm/@types/react-dom@17.0.2/index.d.ts'
         ),
       ]);
+      const tabsState = editorStateService.loadTabsState();
       const elements = await fileService.loadElements();
       const elementMap = R.indexBy(elements, x => x.id);
       elements.forEach(element => {
@@ -79,16 +95,36 @@ export function EditorModule(props: EditorModuleProps) {
           });
         }
       });
+      codeEditor.addEventListener('modified', ({ fileId, hasChanges }) => {
+        setState(draft => {
+          if (hasChanges) {
+            draft.dirtyMap[fileId] = hasChanges;
+          } else {
+            delete draft.dirtyMap[fileId];
+          }
+        });
+      });
+      codeEditor.addEventListener('saved', ({ fileId, content }) => {
+        setState(draft => {
+          delete draft.dirtyMap[fileId];
+        });
+        void fileService.saveFile(fileId, content);
+      });
+      if (tabsState.activeTabId) {
+        codeEditor.openFile(tabsState.activeTabId);
+      }
       setState(draft => {
         draft.isLoaded = true;
         draft.elements = elements;
+        draft.tabs = tabsState.tabs;
+        draft.activeTabId = tabsState.activeTabId;
       });
     },
     openFile: id => {
       const { elements } = getState();
       const file = elements.find(x => x.id === id) as FileInfo;
       setState(draft => {
-        draft.activeFile = id;
+        draft.activeTabId = id;
         if (!draft.tabs.some(x => x.id === id)) {
           draft.tabs.push({
             id: id,
@@ -97,19 +133,21 @@ export function EditorModule(props: EditorModuleProps) {
         }
       });
       codeEditor.openFile(id);
+      syncTabs();
     },
     closeFile: id => {
       let newActiveId: string | null | -1 = -1;
       setState(draft => {
         draft.tabs = draft.tabs.filter(x => x.id !== id);
-        if (draft.activeFile === id) {
-          draft.activeFile = draft.tabs[0]?.id ?? null;
-          newActiveId = draft.activeFile;
+        if (draft.activeTabId === id) {
+          draft.activeTabId = draft.tabs[0]?.id ?? null;
+          newActiveId = draft.activeTabId;
         }
       });
       if (newActiveId !== -1) {
         codeEditor.openFile(newActiveId);
       }
+      syncTabs();
     },
   });
 
--- apps/app/src/features/challenge/editor/EditorTabs.tsx
+++ apps/app/src/features/challenge/editor/EditorTabs.tsx
@@ -2,7 +2,7 @@ import { useEditorActions, useEditorState } from './EditorModule';
 import { FileTab } from './FileTab';
 
 export function EditorTabs() {
-  const { tabs, activeFile } = useEditorState();
+  const { tabs, activeTabId: activeFile, dirtyMap } = useEditorState();
   const { closeFile, openFile } = useEditorActions();
   return (
     <div tw="flex space-x-0.5 overflow-hidden">
@@ -13,6 +13,7 @@ export function EditorTabs() {
           isActive={tab.id === activeFile}
           onOpen={() => openFile(tab.id)}
           onClose={() => closeFile(tab.id)}
+          hasChanges={dirtyMap[tab.id]}
         />
       ))}
     </div>
