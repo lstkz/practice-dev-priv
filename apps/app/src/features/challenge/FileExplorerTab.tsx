71a1b59edb132b1e0885b16ad928b2b29d0e44b0
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2021-08-02 21:08:40 +0200

    IDE: integrate locked files and libraries (#54)

--- apps/app/src/features/challenge/FileExplorerTab.tsx
+++ apps/app/src/features/challenge/FileExplorerTab.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import * as R from 'remeda';
+import { TreeNode } from 'src/types';
 import { FileExplorer } from '../../components/FileExplorer/FileExplorer';
 import {
+  useEditorState,
   useIsEditorLoaded,
   useWorkspaceModel,
   useWorkspaceState,
@@ -9,15 +12,26 @@ import {
 export function FileExplorerTab() {
   const isLoaded = useIsEditorLoaded();
   const { nodes, nodeState } = useWorkspaceState();
+  const { workspace } = useEditorState();
   const workspaceModel = useWorkspaceModel();
+  const lockedNodesMap = React.useMemo(() => {
+    const lockedNodesMap: Record<string, boolean> = {};
+    const nodeMap = R.indexBy(nodes, x => x.id);
+    const travel = (node: TreeNode) => {
+      lockedNodesMap[node.id] = true;
+      if (node.parentId) {
+        travel(nodeMap[node.parentId]);
+      }
+    };
+    workspace.items.filter(x => x.isLocked).forEach(travel);
+    return lockedNodesMap;
+  }, [workspace]);
   if (!isLoaded) {
     return null;
   }
   return (
     <FileExplorer
-      lockedNodesMap={{
-        1: true,
-      }}
+      lockedNodesMap={lockedNodesMap}
       nodeState={nodeState}
       items={nodes}
       onOpenFile={id => workspaceModel.openFile(id)}
