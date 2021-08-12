a38a33da5413329bbf0f7e0e2664ab77e7d50995
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2021-08-12 19:39:00 +0200

    fix

--- apps/app/src/features/module/ModuleFilter.tsx
+++ apps/app/src/features/module/ModuleFilter.tsx
@@ -7,24 +7,54 @@ export function ModuleFilter() {
   return (
     <FilterPanel>
       <FilterSection title="Status">
-        <Checkbox id="status-unattempted" name="status">
+        <Checkbox
+          id="status-unattempted"
+          name="status"
+          checked={false}
+          onChange={() => {}}
+        >
           Unattempted
         </Checkbox>
-        <Checkbox id="status-attempted" name="status">
+        <Checkbox
+          id="status-attempted"
+          name="status"
+          checked={false}
+          onChange={() => {}}
+        >
           Attempted
         </Checkbox>
-        <Checkbox id="status-solved" name="solved">
+        <Checkbox
+          id="status-solved"
+          name="solved"
+          checked={false}
+          onChange={() => {}}
+        >
           Solved
         </Checkbox>
       </FilterSection>
       <FilterSection title="Difficulty">
-        <Checkbox id="difficulty-beginner" name="difficulty">
+        <Checkbox
+          id="difficulty-beginner"
+          name="difficulty"
+          checked={false}
+          onChange={() => {}}
+        >
           Beginner
         </Checkbox>
-        <Checkbox id="difficulty-intermediate" name="difficulty">
+        <Checkbox
+          id="difficulty-intermediate"
+          name="difficulty"
+          checked={false}
+          onChange={() => {}}
+        >
           Intermediate
         </Checkbox>
-        <Checkbox id="difficulty-various" name="difficulty">
+        <Checkbox
+          id="difficulty-various"
+          name="difficulty"
+          checked={false}
+          onChange={() => {}}
+        >
           Various
         </Checkbox>
       </FilterSection>
