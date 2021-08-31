6e73f57b62089066d69735a16db9bb571188b78d
Łukasz Sentkiewicz <lukasz@sentkiewicz.pl>
2021-08-31 11:14:14 +0200

    landing page fixes

--- apps/app/src/features/landing/CryptoSection.tsx
+++ apps/app/src/features/landing/CryptoSection.tsx
@@ -9,6 +9,7 @@ import {
   UsersIcon,
 } from '@heroicons/react/outline';
 import { CRYPTO_LINK } from 'src/config';
+import tw, { styled } from 'twin.macro';
 
 const features = [
   {
@@ -98,67 +99,88 @@ export function CryptoSection2() {
   );
 }
 
-const items = [
-  {
-    id: 1,
-    question: 'Why crypto?',
-    answer: (
-      <>
-        Free platforms still need to have some source of income. One possible
-        way is to ask for donations. Crypto is an alternative that allows
-        raising money quickly, and also it's an investment for users.
-      </>
-    ),
-  },
-  {
-    id: 2,
-    question: 'Can I earn crypto?',
-    answer: (
-      <>
-        Yes! You can earn crypto royalties by contributing to content (e.g.
-        creating challenges or the whole modules) on the platform.
-        <br />
-        Every year we will award from{' '}
-        <strong className="font-semibold">$100,000</strong> to{' '}
-        <strong className="font-semibold">$1,000,000</strong> in crypto to all
-        contributors proportionally.
-      </>
-    ),
-  },
-  {
-    id: 3,
-    question: "What's the token price?",
-    answer: (
-      <>
-        The initial price is $0.01 per token. The number of tokens at this price
-        is limited. <br />
-        We estimate that token will cost $30 per token within a year.
-      </>
-    ),
-  },
-  {
-    id: 4,
-    question: 'Where can I buy it?',
-    answer: (
-      <>
-        You can buy it on{' '}
-        <a
-          target="_blank"
-          href={CRYPTO_LINK}
-          rel="nofollow"
-          className="text-blue-400"
-        >
-          Uniswap.
-        </a>
-        <br />
-        Make sure to buy crypto through this link. Do not open links sent by
-        other people!
-      </>
-    ),
-  },
-];
+const Bold = styled.strong`
+  ${tw`font-semibold text-white`}
+`;
 
 export function CryptoSection() {
+  const items = [
+    {
+      question: 'Why crypto?',
+      answer: (
+        <>
+          Free platforms still need to have some source of income. One possible
+          way is to ask for donations. Crypto is an alternative that allows
+          raising money quickly, and also it's an investment for users.
+        </>
+      ),
+    },
+    {
+      question: 'Can I earn crypto?',
+      answer: (
+        <>
+          Yes! You can earn crypto royalties by contributing to content (e.g.
+          creating challenges or the whole modules) on the platform.
+          <br />
+          Every year we will award from <Bold>$100,000</Bold> to{' '}
+          <Bold>$1,000,000</Bold> in crypto to all contributors proportionally.
+        </>
+      ),
+    },
+    {
+      question: "What's the token price?",
+      answer: (
+        <>
+          The initial price is <Bold>$0.1</Bold> per token.
+          <br />
+          We estimate that token will cost <Bold>$5</Bold> per token within a
+          year.
+        </>
+      ),
+    },
+    {
+      question: "What's the total supply?",
+      answer: (
+        <>
+          There are only <Bold>12,500,000</Bold> tokens. Tokens are non-mintable
+          (it won't be possible to release more tokens).
+        </>
+      ),
+    },
+    {
+      question: "What's the token distribution?",
+      answer: (
+        <>
+          <Bold>80%</Bold> of tokens are available to buy on Uniswap.
+          <br />
+          <Bold>20%</Bold> of tokens are reserved for royalties.
+          <br />
+          There are no tokens reserved for the platform. The platform will make
+          money on Uniswap fees (<Bold>1%</Bold> of all transactions).
+        </>
+      ),
+    },
+    {
+      question: 'Where can I buy it?',
+      answer: (
+        <>
+          You can buy it on{' '}
+          <a
+            target="_blank"
+            href={CRYPTO_LINK}
+            rel="nofollow"
+            className="text-blue-400"
+          >
+            Uniswap.
+          </a>
+          <br />
+          Make sure to buy crypto through this link. Do not open links sent by
+          other people!
+        </>
+      ),
+    },
+  ];
+
   return (
     <div className="bg-gray-900">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
@@ -172,8 +194,8 @@ export function CryptoSection() {
         </div>
         <div className="mt-20">
           <dl className="space-y-10 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-x-8 lg:gap-y-10">
-            {items.map(faq => (
-              <div key={faq.id}>
+            {items.map((faq, i) => (
+              <div key={i}>
                 <dt className="font-semibold text-white text-lg">
                   {faq.question}
                 </dt>
--- apps/app/src/features/landing/FAQSection.tsx
+++ apps/app/src/features/landing/FAQSection.tsx
@@ -5,79 +5,90 @@ import tw, { styled } from 'twin.macro';
 
 const CodeWrapper = styled.div`
   code {
-    ${tw`bg-indigo-50 text-indigo-400 px-2 rounded-sm`}
+    ${tw`bg-indigo-50 text-indigo-500 px-2 rounded-sm leading-loose`}
   }
 `;
 
-const faqs = [
-  {
-    id: 1,
-    question: 'Is it really free?',
-    answer: (
-      <>
-        Yes! You can solve all challenges for free without any limits.
-        <br />
-        <strong className="font-semibold">
-          You don't have to buy any crypto to use the website!
-        </strong>
-      </>
-    ),
-  },
-  {
-    id: 2,
-    question: 'What languages and technologies do you support?',
-    answer:
-      'Currently, only React and Typescript. More stacks will be added in the future.',
-  },
-  {
-    id: 3,
-    question: 'Is there only frontend?',
-    answer:
-      'Yes, but backend technologies are planned to be added in the future.',
-  },
-  {
-    id: 4,
-    question: 'How can I add my module or challenges?',
-    answer: (
-      <>
-        Please contact us{' '}
-        <Link href={createUrl({ name: 'contact-us' })}>
-          <a>here</a>
-        </Link>
-        .
-        <br />
-        We highly encourage open-source maintainers from popular frameworks or
-        libraries to contribute.
-      </>
-    ),
-  },
-  {
-    id: 5,
-    question: 'How will crypto royalties be calculated?',
-    answer: (
-      <CodeWrapper>
-        Royalties will be calculated proportionally based on the module or
-        challenge popularity.
-        <br />
-        Example:
-        <br />
-        Assume there are <code>100,000</code> users, <code>500</code>{' '}
-        challenges, and every user participates in <code>40</code> challenges on
-        average.
-        <br />
-        That gives us <code>4,000,000</code> participants on the whole platform.
-        <br />
-        If your challenges have <code>400,000</code> participants, then you will
-        receive <code>$POOL * 400,000/4,000,000</code>.
-        <br />
-        If <code>$POOL</code> is <code>$1,000,000</code> then you would get{' '}
-        <code>$100,000</code>.
-      </CodeWrapper>
-    ),
-  },
-];
+const List = styled.ul`
+  ${tw`mb-2 list-disc ml-8`}
+`;
 
 export function FAQSection() {
+  const faqs = [
+    {
+      id: 1,
+      question: 'Is it really free?',
+      answer: (
+        <>
+          Yes! You can solve all challenges for free without any limits.
+          <br />
+          <strong className="font-semibold text-gray-900">
+            You don't have to buy any crypto to use the website!
+          </strong>
+        </>
+      ),
+    },
+    {
+      id: 2,
+      question: 'What languages and technologies do you support?',
+      answer:
+        'Currently, only React and Typescript. More stacks will be added in the future.',
+    },
+    {
+      id: 3,
+      question: 'Is there only frontend?',
+      answer:
+        'Yes, but backend technologies are planned to be added in the future.',
+    },
+    {
+      id: 4,
+      question: 'How can I add my module or challenges?',
+      answer: (
+        <>
+          Please contact us{' '}
+          <Link href={createUrl({ name: 'contact-us' })}>
+            <a>here</a>
+          </Link>
+          .
+          <br />
+          We highly encourage open-source maintainers from popular frameworks or
+          libraries to contribute.
+        </>
+      ),
+    },
+    {
+      id: 5,
+      question: 'How will crypto royalties be calculated?',
+      answer: (
+        <CodeWrapper>
+          Royalties will be calculated proportionally based on the module or
+          challenge popularity.
+          <div tw="italic my-2">Example:</div>
+          Assume there are:
+          <List>
+            <li>
+              <code>100,000</code> users,
+            </li>
+            <li>
+              <code>500</code> challenges,
+            </li>
+            <li>
+              every user participates in <code>40</code> challenges on average.
+            </li>
+          </List>
+          That gives us <code>4,000,000</code> participants on the whole
+          platform.
+          <br />
+          If your challenges have <code>400,000</code> participants, then you
+          will receive: <br /> <code>$POOL * 400,000/4,000,000</code>.
+          <br />
+          If <code>$POOL</code> is <code>$1,000,000</code> then you would get{' '}
+          <code>$100,000</code>.
+        </CodeWrapper>
+      ),
+    },
+  ];
+
   return (
     <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-20 lg:px-8">
       <div className="lg:grid lg:grid-cols-3 lg:gap-8">
